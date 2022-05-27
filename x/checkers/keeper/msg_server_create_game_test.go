fa4298a9f1feb2504e285a2e87eab23423260dd4
Yuhan Wang <yuhanwang@Yuhans-MacBook-M1.local>
2022-05-27 09:47:12 -0400

    update msg server create game

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,9 +1,13 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/keeper"
 	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,6 +16,12 @@ const (
 	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
+
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := setupKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
 
 func TestCreateGame(t *testing.T) {
 	msgServer, context := setupMsgServer(t)
